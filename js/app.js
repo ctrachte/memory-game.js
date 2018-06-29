a7e5385de53d7d7fe64e4f9eb9162db34e6a0a7d
Caleb Trachte <cetrachte@hotmail.com>
2018-06-29 15:41:47 -0500

    add working conditions for match to click event listener fix bug in cardIcons array

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(window).on('load', resetBoard);
 
  function resetBoard () {
-  let cardIcons = ["fa fa-diamond", "fa fa-cube","fa fa-bolt","fa fa-leaf","fa fa-bicycle","fa fa-paper-plane-o"]; // our deck of card icons
-  let cardSets = shuffle(cardIcons).concat(shuffle(cardIcons)); // create sets by shuffling two decks together
+  let cardIcons = ["fa fa-diamond", "fa fa-cube","fa fa-bolt","fa fa-leaf","fa fa-bicycle","fa fa-paper-plane-o","fa fa-diamond",
+   "fa fa-cube","fa fa-bolt","fa fa-leaf","fa fa-bicycle","fa fa-paper-plane-o"]; // our deck of card icons
+  let cardSets = shuffle(cardIcons); // shuffles deck
   // For each card in the deck, add a random icon.
    $('#deck').children('li').each(function (index) {
      this.children[0].className = cardSets[index];
@@ -23,26 +24,38 @@ function shuffle(array) {
 
     return array;
 }
+// tracks how many cards you've flipped.
+let cardsFlipped = [];
+// tracks matched sets
+let matches = [];
 
-let cardsFlipped = 0;
-
-const hideCards = () => {
-  $('.card').removeClass('show');
-  $('.card').removeClass('open');
-  cardsFlipped = 0;
+// hides card icons (flips them back)
+let hideCards = () => {
+  $('.flippable').removeClass('show');
+  $('.flippable').removeClass('open');
+  cardsFlipped = [];
 }
 
- $('.card').click(function () {
-   if (cardsFlipped===0) {
-     setTimeout(hideCards, 1000);
-     cardsFlipped++;
+// Card click event handler
+ $('.flippable').click(function () {
+   if (cardsFlipped.length===0) {
+     // setTimeout(hideCards, 1000);
+     cardsFlipped.push(this.children[0].className);
      $(this).addClass('show');
      $(this).addClass('open');
-   } else if (cardsFlipped===1) {
-     cardsFlipped++;
+   } else if (cardsFlipped.length===1) {
+     cardsFlipped.push(this.children[0].className);
      $(this).addClass('show');
      $(this).addClass('open');
    } else {
-     hideCards();
+     if (cardsFlipped[0] === cardsFlipped[1]) {
+       console.log(cardsFlipped);
+       $('.open').removeClass('flippable');
+       matches.push(cardsFlipped);
+       cardsFlipped = [];
+       return;
+     } else {
+       hideCards();
+     }
    }
  })
